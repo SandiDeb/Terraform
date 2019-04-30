2a9d13e9dca0652237a09b12ca300738042f281d
Deb <sandipan.deb@capgemini.com>
2019-04-30 13:28:16 +0100

    Changed terraform.tf

--- Terraform.tf
+++ Terraform.tf
@@ -57,7 +57,7 @@ resource "azurerm_network_security_group" "myterraformnsg" {
         direction                  = "Inbound"
         access                     = "Allow"
         protocol                   = "Tcp"
-        source_port_range          = "203.191.35.0/24"
+        source_ip_adresses         = "203.191.35.0/24"
         destination_port_range     = "22"
         source_address_prefix      = "*"
         destination_address_prefix = "*"
